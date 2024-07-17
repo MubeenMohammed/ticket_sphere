f9e161567971b0f23d82d76dd175862615ad6a33
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-07-17 15:04:11 -0400

    created the signUp page

--- lib/app_pages/login_section/login_section_container.dart
+++ lib/app_pages/login_section/login_section_container.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_project/app_pages/login_section/login_page.dart';
+import 'package:new_project/app_pages/login_section/signup_page.dart';
 
 class LoginSectionContainer extends StatefulWidget {
   const LoginSectionContainer({super.key});
@@ -10,124 +12,34 @@ class LoginSectionContainer extends StatefulWidget {
 }
 
 class _LoginSectionContainerState extends State<LoginSectionContainer> {
-  bool _obscureText = true;
+  String activeLoginScreen = 'Login-page';
 
-  bool _isRememberMe = false;
+  void switchScreen(String screen) {
+    setState(() {
+      activeLoginScreen = screen;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            'assets/login_background.jpg',
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/login_background.jpg',
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 250,
-          ),
-          const Text(
-            "Sign In",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            "Welcome back",
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-          ),
-          const Text(
-            "Sign in to continue with existing account",
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-          ),
-          const SizedBox(height: 25),
-          const TextField(
-            decoration: InputDecoration(
-              hintText: "Email",
-              hintStyle: TextStyle(
-                color: Color.fromARGB(255, 196, 190, 190),
-              ),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Color.fromARGB(255, 194, 190, 190),
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-              ),
-              filled: true,
-              fillColor: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Password",
-              hintStyle: const TextStyle(
-                color: Color.fromARGB(255, 196, 190, 190),
-              ),
-              prefixIcon: const Icon(
-                Icons.lock,
-                color: Color.fromARGB(255, 194, 190, 190),
-              ),
-              border: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-              ),
-              filled: true,
-              fillColor: Colors.white,
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _obscureText = !_obscureText;
-                  });
-                },
-                icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility),
-              ),
-            ),
-            obscureText: _obscureText,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: _isRememberMe,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _isRememberMe = value!;
-                      });
-                    },
-                  ),
-                  const Text("Remember me"),
-                ],
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Forgot Password",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 40),
-          
-        ],
-      ),
-    );
+        child: currentActiveLoginScreen());
+  }
+
+  Widget currentActiveLoginScreen() {
+    if (activeLoginScreen == "SignUp-Page") {
+      return const SignupPage();
+    } else {
+      return LoginPage(switchLoginScreen: switchScreen);
+    }
   }
 }
--- lib/app_pages/login_section/signup_page.dart
+++ lib/app_pages/login_section/signup_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key, required this.switchLoginScreen});
-
-  final Function switchLoginScreen;
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
 
   @override
-  State<LoginPage> createState() {
-    return _LoginPageState();
+  State<SignupPage> createState() {
+    return _SignupPageState();
   }
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
   bool _obscureText = true;
 
   bool _isRememberMe = false;
@@ -22,10 +20,10 @@ class _LoginPageState extends State<LoginPage> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(
-          height: 200,
+          height: 150,
         ),
         const Text(
-          "Sign In",
+          "Sign Up",
           style: TextStyle(
             color: Colors.black,
             fontSize: 30,
@@ -34,14 +32,34 @@ class _LoginPageState extends State<LoginPage> {
         ),
         const SizedBox(height: 10),
         const Text(
-          "Welcome back",
+          "Welcome",
           style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
         ),
         const Text(
-          "Sign in to continue with existing account",
+          "Sign up to create new account",
           style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
         ),
         const SizedBox(height: 25),
+        const TextField(
+          decoration: InputDecoration(
+            hintText: "Full Name",
+            hintStyle: TextStyle(
+              color: Color.fromARGB(255, 196, 190, 190),
+            ),
+            prefixIcon: Icon(
+              Icons.person,
+              color: Color.fromARGB(255, 194, 190, 190),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+            ),
+            filled: true,
+            fillColor: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 10),
         const TextField(
           decoration: InputDecoration(
             hintText: "Email",
@@ -91,40 +109,45 @@ class _LoginPageState extends State<LoginPage> {
           ),
           obscureText: _obscureText,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Checkbox(
-                  value: _isRememberMe,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _isRememberMe = value!;
-                    });
-                  },
-                ),
-                const Text("Remember me"),
-              ],
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Confirm Password",
+            hintStyle: const TextStyle(
+              color: Color.fromARGB(255, 196, 190, 190),
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Forgot Password",
-                style: TextStyle(color: Colors.white),
+            prefixIcon: const Icon(
+              Icons.lock,
+              color: Color.fromARGB(255, 194, 190, 190),
+            ),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
               ),
             ),
-          ],
+            filled: true,
+            fillColor: Colors.white,
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+              icon:
+                  Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
+            ),
+          ),
+          obscureText: _obscureText,
         ),
         const SizedBox(height: 30),
         ElevatedButton(
           onPressed: () {},
           style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 160, vertical: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 155, vertical: 15),
             backgroundColor: const Color.fromARGB(255, 3, 205, 199),
           ),
           child: const Text(
-            "Sign In",
+            "Sign Up",
             style: TextStyle(fontSize: 16, color: Colors.white),
           ),
         ),
@@ -136,20 +159,6 @@ class _LoginPageState extends State<LoginPage> {
             color: Colors.white,
           ),
           textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            widget.switchLoginScreen("SignUp-Page");
-          },
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 102, vertical: 15),
-            backgroundColor: const Color.fromARGB(255, 3, 205, 199),
-          ),
-          child: const Text(
-            "Create a new Account",
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
         ),
         const SizedBox(height: 20),
         OutlinedButton(
@@ -166,6 +175,20 @@ class _LoginPageState extends State<LoginPage> {
               fontSize: 16,
             ),
           ),
+        ),
+        const SizedBox(height: 10),
+        const Row(
+          children: [
+            Text(
+              "Already have account?",
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(width: 10),
+            Text(
+              "Login",
+              style: TextStyle(color: Color.fromARGB(255, 32, 192, 128)),
+            )
+          ],
         )
       ],
     );
